b05fdbd68f85f7e40acdb7ffaea3898713e5da21
Thiago Abrantes <abrantes.souza@gmail.com>
2016-12-21 17:34:55 -0200

    Adicionando redirecionamento para android

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 
+var os = navigator.userAgent.toLowerCase();
+var isAndroid = os.indexOf("android") > -1; //&& ua.indexOf("mobile");
+if(isAndroid) {
+ alert("Desculpe, esse navegador ainda não é suportado para esse app!!")
+ window.location("https://br.linkedin.com/in/thiagoabrantescsouza");
+}
+
 document.title=">>Thiago Abrantes";
 var game = new Phaser.Game(800 , 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
 var cursors;
